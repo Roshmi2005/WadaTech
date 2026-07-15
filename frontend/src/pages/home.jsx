b74c5a218a17f40d64030e3f54b950d5bdfdaf77
pra-kriti8 <prakritistha2005@gmail.com>
2026-07-15 21:39:52 +0545

    pushing frontend

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Digital Ward Management System</h1>
+      <p>Welcome to the Digital Ward Management System</p>
+    </div>
+  );
+}
+
+export default Home;
